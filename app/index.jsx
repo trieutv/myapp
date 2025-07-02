f35e176e841c33d45290e7d98d5cb6d1f4eb40da
trieu.tvit <trinhvantrieu39@gmail.com>
2025-07-02 13:59:56 +0700

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native"
+
+
+const Home = () => {
+  return (
+    <View>
+        <Text>Welcome to the Home Page</Text>
+    </View>
+  );
+}
+export default Home;
+const styles = StyleSheet.create({})
